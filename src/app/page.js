04b32dfb20be61efa504a67ea2b8814414e3e0d6
unknown <asim26k@gmail.com>
2025-11-29 13:24:24 +0530

    Align and fix line spacin in Hero Section

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,8 @@ import { RoughNotation } from "react-rough-notation";
 export default function Home() {
   return (
     <div>
-      <section className=" tracking-wide ">
-        <div className="  min-h-screen   flex flex-col justify-center items-center px-12 md:px-24">
+      <section className=" md:tracking-wider  md:leading-2 ">
+        <div className="  py-24 md:py-64   flex flex-col justify-center items-center text-center px-12 md:px-24">
           <h1 className=" text-3xl md:text-6xl font-bold font-display tracking-tight bg-red-400 bg-clip-text">
             Smarter Learning, Happier Kids.
           </h1>
@@ -37,13 +37,24 @@ export default function Home() {
               boring lectures{" "}
             </RoughNotation>
             lectures, no{" "}
-            <span className="text-red-400">stressful commutes.</span>
+            <RoughNotation
+              type="circle"
+              show="true"
+              color="oklch(63.7% 0.237 25.331)"
+              padding={3}
+            >
+              stressful commutes{" "}
+            </RoughNotation>
           </h2>
 
-          <button className="bg-sky-500 px-6 p-2 rounded-full mt-3 text-neutral-50">
+          <button className="bg-sky-500 px-12 p-4 rounded-full mt-3 text-neutral-50">
             Explore EdRev
           </button>
         </div>
+      </section>
+
+      <section>
+        <h2>Section 2</h2>
       </section>
     </div>
   );
